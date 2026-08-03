b4f594929a7f1980cc687d86ada27b355b64e803
lianai7799-star <lianai7799@gmail.com>
2026-08-03 22:50:37 +0800

    Update oauth-authorization-server.js

--- api/well-known/oauth-authorization-server.js
+++ api/well-known/oauth-authorization-server.js
@@ -1,11 +1,26 @@
 export default function handler(req, res) {
   const host = `https://${req.headers.host}`;
+
   res.status(200).json({
     issuer: host,
+
     authorization_endpoint: `${host}/api/oauth/authorize`,
+
     token_endpoint: `${host}/api/oauth/token`,
+
+    registration_endpoint: `${host}/api/oauth/register`,
+
     response_types_supported: ["code"],
-    grant_types_supported: ["authorization_code", "refresh_token"],
-    code_challenge_methods_supported: ["S256"]
+
+    grant_types_supported: [
+      "authorization_code",
+      "refresh_token"
+    ],
+
+    code_challenge_methods_supported: ["S256"],
+
+    token_endpoint_auth_methods_supported: [
+      "none"
+    ]
   });
 }
